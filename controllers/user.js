a3bc183bc68149a308c4f7550b6eebf8e932593d
Raj Gupta <rgm5823@gmail.com>
2021-08-28 19:57:23 +0530

    First Commit

---
+++ controllers/user.js
@@ -0,0 +1,36 @@
+const User = require("../models/user");
+
+//middleware
+exports.getUserById = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "No user was found in DB"
+      });
+    }
+    req.profile = user;
+    next();
+  });
+};
+
+exports.getUser = (req, res) => {
+  req.profile.password = undefined;
+  return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user"
+        });
+      }
+      user.password = undefined;
+      res.json(user);
+    }
+  );
+};
---
+++ routes/product.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+  getAllProducts,
+  getAllUniqueCategory
+} = require("../controllers/product");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
+
+//all of params
+router.param("userId", getUserById);
+router.param("productId", getProductById);
+
+//all of actual routes
+//create route
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
+
+// read routes
+router.get("/product/:productId", getProduct);
+
+//delete route
+router.delete(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteProduct
+);
+
+//update route
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+//listing route
+router.get("/products", getAllProducts);
+router.get("/product/categories" , getAllUniqueCategory)
+
+module.exports = router;
